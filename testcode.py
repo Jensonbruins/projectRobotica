8028630c493459d5b25bd26539b11ead6992f6ac
Jenson <jenson@jensonbruins.nl>
2021-05-25 13:38:58 +0200

    feat: added 'target detection'

--- testcode.py
+++ testcode.py
@@ -6,6 +6,17 @@ from letterDetection import letterDetection
 
 cap = cv2.VideoCapture(0)
 
+def nothing(x):
+    pass
+
+cv2.namedWindow('control')
+
+cv2.createTrackbar('minLineLength','control',0,255,nothing)
+cv2.setTrackbarPos('minLineLength', 'control', 7)
+cv2.createTrackbar('maxLineGap','control',0,255,nothing)
+cv2.setTrackbarPos('maxLineGap', 'control', 4)
+cv2.createTrackbar('threshold','control',0,255,nothing)
+cv2.setTrackbarPos('threshold', 'control', 11)
 #
 # TODO: Find a fix for global variable 'previousLastNumber'
 #
@@ -15,6 +26,37 @@ paper = paperDetection()
 letter = letterDetection()
 averageArray = []
 while(True):
+    letterArray = [
+    #   ['letter', horizontalMin, horizontalMax, verticalMin, verticalMax, diagonalMin, diagonalMax]
+        ['A', 1, 2, 0, 0.2, 7.5, 9.1],
+        ['B', 5.5, 6.5, 4.5, 7, 2, 3],
+        ['C', 0, 0, 0, 0, 0, 0],
+        ['D', 3, 4.5, 3.5, 5, 2.5, 3.5],
+        # ['E', 0, 0, 0, 0, 0, 0],
+        # ['F', 0, 0, 0, 0, 0, 0],
+        # ['G', 0, 0, 0, 0, 0, 0],
+        # ['H', 0, 0, 0, 0, 0, 0],
+        # ['I', 0, 0, 0, 0, 0, 0],
+        # ['J', 0, 0, 0, 0, 0, 0],
+        # ['K', 0, 0, 0, 0, 0, 0],
+        # ['L', 0, 0, 0, 0, 0, 0],
+        # ['M', 0, 0, 0, 0, 0, 0],
+        # ['N', 0, 0, 0, 0, 0, 0],
+        # ['O', 0, 0, 0, 0, 0, 0],
+        # ['P', 0, 0, 0, 0, 0, 0],
+        # ['Q', 0, 0, 0, 0, 0, 0],
+        # ['R', 0, 0, 0, 0, 0, 0],
+        # ['S', 0, 0, 0, 0, 0, 0],
+        # ['T', 0, 0, 0, 0, 0, 0],
+        # ['U', 0, 0, 0, 0, 0, 0],
+        # ['V', 0, 0, 0, 0, 0, 0],
+        # ['W', 0, 0, 0, 0, 0, 0],
+        # ['X', 0, 0, 0, 0, 0, 0],
+        # ['Y', 0, 0, 0, 0, 0, 0],
+        # ['Z', 0, 0, 0, 0, 0, 0],
+    ]
+
+
     ret, frame = cap.read()
 
     paper.update(frame)
@@ -25,6 +67,10 @@ while(True):
     #
     # Creating the images of the detected letters
 
+    minLineLengthVar = cv2.getTrackbarPos('minLineLength','control')
+    maxLineGapVar = cv2.getTrackbarPos('maxLineGap','control')
+    thresholdVar = cv2.getTrackbarPos('threshold','control')
+
     lastNumber = 0
     twodarray = []
     # loop over every letter
@@ -32,13 +78,13 @@ while(True):
         # edit frames to make letters more clear
         temporaryFrame = paper.get()[x[3]:x[1], x[0]:x[2]]
         blur = cv2.medianBlur(temporaryFrame,5)
-        blur = cv2.bilateralFilter(blur,9,75,75)
+        # blur = cv2.bilateralFilter(temporaryFrame,5,75,75)
         cannyFrame = cv2.Canny(blur,200,255)
         # important for cleanup
         lastNumber = index
 
         # Check for lines in image
-        lines = cv2.HoughLinesP(cannyFrame, 1, np.pi / 180, 9, minLineLength=5, maxLineGap=5)
+        lines = cv2.HoughLinesP(cannyFrame, 1, np.pi / 180, thresholdVar, minLineLength=minLineLengthVar, maxLineGap=maxLineGapVar)
         # if there are lines in image
         if lines is not None:
             horizontal = 0
@@ -70,15 +116,30 @@ while(True):
                 averageArray.insert(index, [0, horizontal, vertical, diagonal])
 
         cv2.imshow('t' + str(index), temporaryFrame)
-        # cv2.imshow('a' + str(index), blur)
-        # cv2.imshow('c' + str(index), cannyFrame)
+        cv2.imshow('a' + str(index), blur)
+        cv2.imshow('c' + str(index), cannyFrame)
 
     for x in averageArray:
-        if x[0] == 20:
-            print(averageArray)
+        if x[0] == 10:
+            # print(averageArray)
             for a in averageArray:
-                if a[0] != 0:
-                    print('Horizontal: ', a[1]/a[0], 'Vertical: ', a[2]/a[0], 'Diagonal: ', a[3]/a[0])
+                if a[0] > 5:
+                    horizontalAvg = a[1]/a[0]
+                    verticalAvg = a[2]/a[0]
+                    diagonalAvg = a[3]/a[0]
+                    print('Horizontal: ', horizontalAvg, 'Vertical: ', verticalAvg, 'Diagonal: ', diagonalAvg)
+
+                    for l in letterArray:
+                        # print(l[0])
+                        # print(l[1], horizontalAvg, l[2])
+                        if l[1] <= horizontalAvg <= l[2]:
+                            # print('worked')
+                            # print(l[3], verticalAvg, l[4])
+                            if l[3] <= verticalAvg <= l[4]:
+                                # print('worked1')
+                                if l[5] <= diagonalAvg <= l[6]:
+                                    print('target = ', l[0])
+                                    break;
             averageArray = []
             break
 
@@ -88,8 +149,8 @@ while(True):
     #
     for x in range(lastNumber + 1, previousLastNumber + 1):
         cv2.destroyWindow('t'+str(x))
-        # cv2.destroyWindow('a'+str(x))
-        # cv2.destroyWindow('c'+str(x))
+        cv2.destroyWindow('a'+str(x))
+        cv2.destroyWindow('c'+str(x))
 
     previousLastNumber = lastNumber
 
